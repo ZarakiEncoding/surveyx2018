132b91e2b3fc6a77f5cd4c8f62c0ea57ca52dc49
Miguel <miguelgarciadeprada@gmail.com>
2017-12-20 13:42:18 +0100

    Checkbox created

--- Surveyx/app/src/main/java/es/academy/solidgear/surveyx/ui/activities/LoginActivity.java
+++ Surveyx/app/src/main/java/es/academy/solidgear/surveyx/ui/activities/LoginActivity.java
@@ -1,8 +1,12 @@
 package es.academy.solidgear.surveyx.ui.activities;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.AppCompatCheckBox;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
